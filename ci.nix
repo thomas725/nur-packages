a1dfa867e287c96dcf0b3e457274f8a78f63bac8
thomas725 <thomas85@mail.ch>
2026-02-06 15:25:53 +0100

    setup cachix

--- ci.nix
+++ ci.nix
@@ -1,4 +1,15 @@
-{ pkgs ? import <nixpkgs> {} }:
 {
-  cacheOutputs = [];
+  pkgs ? import <nixpkgs> { },
+}:
+{
+  cacheOutputs = [
+    nurPkgs.beurer_bf100_parser
+    nurPkgs.npupnp
+    nurPkgs.libupnpp
+    nurPkgs.upplay
+    nurPkgs.eezupnp
+    nurPkgs.betterbird-bin
+    nurPkgs.czkawka-git
+    nurPkgs.birt-designer
+  ];
 }
